1021562c0075523e7a52bc4712881aa37b85c37b
Linjia Zhou <44042122+Linjia-Zhou@users.noreply.github.com>
2026-04-18 14:51:50 -0400

    Add: reverse-integer - submission-1

---
+++ Data Structures & Algorithms/reverse-integer/submission-1.py
@@ -0,0 +1,19 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        max_int = 0x7FFFFFFF
+        min_int = -0x80000000
+        ans = 0
+
+        str_x = str(x)
+
+        if x < 0: 
+            str_x = str_x[::-1]
+            ans = -int(str_x[:-1])
+        else:
+            ans = int(str_x[::-1])
+        
+        if ans < min_int or ans > max_int: return 0
+        else: return ans
+
+
+        
