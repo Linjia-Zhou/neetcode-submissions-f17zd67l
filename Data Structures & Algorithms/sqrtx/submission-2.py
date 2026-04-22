eb193f2cc292fa2cf50c60a8cb298b52e1ab432c
Linjia Zhou <44042122+Linjia-Zhou@users.noreply.github.com>
2026-04-22 15:54:44 -0400

    Add: sqrtx - submission-2

---
+++ Data Structures & Algorithms/sqrtx/submission-2.py
@@ -0,0 +1,22 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        l, r = 0, x
+        ans = 0
+
+        while l <= r:
+            m = (l + r) // 2
+            
+            if m * m == x: return m
+            
+            if m * m > x:
+                r = m - 1
+            
+            if m * m < x:
+                ans = m
+                l = m + 1
+
+        return ans
+
+
+
+
