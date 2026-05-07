2b23c9b8b1b14718056dedcc5a766fad5f5bc484
Linjia Zhou <44042122+Linjia-Zhou@users.noreply.github.com>
2026-05-07 15:45:50 -0400

    Add: arranging-coins - submission-2

---
+++ Data Structures & Algorithms/arranging-coins/submission-2.py
@@ -0,0 +1,16 @@
+class Solution:
+    def arrangeCoins(self, n: int) -> int:
+        l, r = 0, n
+
+        while l <= r:
+            m = (l + r) // 2 # test this as the output
+            summ = (m * (m + 1)) / 2
+            if summ == n:
+                return m
+            elif summ > n:
+                r = m - 1
+            else:
+                l = m + 1
+        
+        return (l + r) // 2
+
